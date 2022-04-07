40bbdc593c62b4db507a99bac6c126926fc198b0
Ilya Shishkin <mostbook@mail.ru>
2022-04-08 00:01:24 +0700

    added useEffect for setting the isAuth status

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import {BrowserRouter} from "react-router-dom";
 import './scss/style.scss';
 import {RouterComponent} from "./components/routerComponent";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {getAuthStatus} from "./store/asyncActions";
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAuthStatus());
+    }, []);
 
     return (
         <div>
