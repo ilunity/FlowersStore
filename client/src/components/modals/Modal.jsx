919ea031dc62862869851f470f8fa502e0279fa3
Ilya Shishkin <mostbook@mail.ru>
2022-04-13 15:31:41 +0700

    changed style files paths

--- client/src/components/modals/Modal.jsx
+++ client/src/components/modals/Modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../scss/components/modal/modal.scss';
+import '../../scss/components/modal.scss';
 
 function Modal({isActive, hideModal, children}) {
     const onWindowClick = () => {
--- client/src/components/modals/Modals.jsx
+++ client/src/components/modals/Modals.jsx
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import {RegistrationModal} from "./Registration/RegistrationModal";
 import {LoginModal} from "./Login/LoginModal";
 
+
 function Modals(props) {
     return (
         <Fragment>
