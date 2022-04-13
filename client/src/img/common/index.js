044b23e21d3fa362620a74e8c3ce5188efd27f7a
Ilya Shishkin <mostbook@mail.ru>
2022-04-13 15:24:39 +0700

    reorganization img files

---
+++ client/src/img/common/index.js
@@ -0,0 +1,20 @@
+import arrowDown from './arrow-down.svg';
+import arrowUp from './arrow-up.svg';
+import arrowRight from './arrow-up.svg';
+import checkmark from './checkmark.svg';
+import arrowDownTight from './arrow-down-tight.svg';
+import arrowUpTight from './arrow-up-tight.svg';
+import arrowLeftTight from './arrow-left-tight.svg';
+import arrowRightTight from './arrow-right-tight.svg';
+
+
+export {
+    arrowDown,
+    arrowUp,
+    arrowRight,
+    checkmark,
+    arrowDownTight,
+    arrowUpTight,
+    arrowLeftTight,
+    arrowRightTight,
+};
