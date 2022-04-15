41784a9791c45c38e0d22153651ea7fae9cc6677
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-04-15 16:08:28 +0300

    added banner, section about us to the main page

---
+++ client/src/img/home/index.js
@@ -0,0 +1,9 @@
+import photo from "./photo.jpg";
+import present from "./present.jpg";
+import video from "./video.jpg";
+import boxes from "./boxes.jpg";
+import baskets from "./baskets.jpg";
+import gifts from "./gifts.jpg";
+import catolog from "./catolog.jpg";
+import roses from "./roses.jpg";
+export {photo, present, video, boxes, baskets, gifts, catolog, roses};
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
-
+import {photo, present, video, boxes, baskets, gifts, catolog, roses} from "../img/home/index";
 const Home = () => {
     return (
         <main className='main'>
-            
+            <div className="main__home home">
+                <div className="home__container container">
+                    <div className="home__banner banner">
+                        <div className="banner__column">
+                            <div className="banner__discount">
+                                <img src={catolog} alt="" />
+                            </div>
+                        </div>
+                        <div className="banner__column">
+                            <div className="banner__catalog catalog">
+                                <div className="catalog__row">
+                                    <div className="catalog__item">
+                                        <img src={roses} alt="" />
+                                    </div>
+                                    <div className="catalog__item">
+                                        <img src={gifts} alt="" />
+                                    </div>
+                                </div>
+                                <div className="catalog__row">
+                                    <div className="catalog__item">
+                                        <img src={baskets} alt="" />
+                                    </div>
+                                    <div className="catalog__item">
+                                        <img src={boxes} alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="home__about about">
+                        <div className="about__item">
+                            <img src={photo} alt="" />
+                        </div>
+                        <div className="about__item">
+                            <img src={video} alt="" />
+                        </div>
+                        <div className="about__item">
+                            <img src = {present} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 };
