42f9dc82d9bededb2ab6f71a4524de9b5ecbf82f
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-04-20 00:26:56 +0300

    added paths to information pages

--- client/src/utils/routes.js
+++ client/src/utils/routes.js
@@ -1,20 +1,39 @@
-import Basket from "../pages/Basket"
-import Item from "../pages/Item"
-import Catalog from "../pages/Catalog"
-import Order from "../pages/Order"
-import Home from "../pages/Home"
+import Basket from "../pages/Basket";
+import Item from "../pages/Item";
+import Catalog from "../pages/Catalog";
+import Order from "../pages/Order";
+import Home from "../pages/Home";
+import About from "../pages/info/About";
+import Сontacts from "../pages/info/Сontacts";
+import PaymentDelivery from "../pages/info/PaymentDelivery";
+
 const paths = {
     HOME: "/",
     ITEM: "/item/:id",
     CATALOG: "/catalog",
     BASKET: "/basket",
     ORDER: "/order",
+    ABOUT: "/about",
+    СONTACTS: "/contacts",
+    PAYMENT_DELIVERY: "/payment_delivery,"
 };
 
 const routes = [
     {
         path: paths.ITEM,
         component: Item,
+    },
+    {
+        path: paths.ABOUT,
+        component: About,
+    },
+    {
+        path: paths.СONTACTS,
+        component: Сontacts,
+    },
+    {
+        path: paths.PAYMENT_DELIVERY,
+        component: PaymentDelivery,
     },
     {
         path: paths.CATALOG,
