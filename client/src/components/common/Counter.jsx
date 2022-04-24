fe500b238f33a24ff53425239882fd70c0e73d81
Ilya Shishkin <mostbook@mail.ru>
2022-04-24 15:05:27 +0700

    configured Counter callback in ItemBasket

--- client/src/components/common/Counter.jsx
+++ client/src/components/common/Counter.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import addCount from '../../img/common/arrow-right-tight.svg';
 import removeCount from '../../img/common/arrow-left-tight.svg';
-import { setItemCount } from '../../http/basketAPI';
 
 
-const Counter = ({count, setCount, maxCount, className = '', id}) => {
-    const increaseCount = async (id, count) => {
+const Counter = ({count, setCount, maxCount, className = ''}) => {
+    const increaseCount = (count) => {
         if (count < maxCount) {
             setCount(count + 1);
-            const currentCount = count + 1;
-            // await setItemCount(id, currentCount);
         }
-    }
-    const decreaseCount = async (id, count) => {
+    };
+    const decreaseCount = (count) => {
         if (count > 1) {
             setCount(count - 1);
-            const currentCount = count - 1
-            // await setItemCount(id, currentCount);
         }
-    }
+    };
+
     return (
         <div className={`counter ${className}`}>
             <img
-                onClick={() => decreaseCount(id, count)}
+                onClick={() => decreaseCount(count)}
                 className='counter__arrow counter__arrow-del'
                 src={removeCount}
                 alt="decreaseCount"
             />
             {count}
             <img
-                onClick={() => increaseCount(id, count)}
+                onClick={() => increaseCount(count)}
                 className='counter__arrow counter__arrow-add'
                 src={addCount}
                 alt="increaseCount"
--- client/src/components/common/ItemBasket.jsx
+++ client/src/components/common/ItemBasket.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {deleteFromBasket} from '../../http/basketAPI';
+import {deleteFromBasket, setItemCount} from '../../http/basketAPI';
 import {STATIC_URL} from '../../http/consts';
 import deleteIcon from '../../img/basket/delete.svg';
 import {deleteItemBasket} from '../../store/actions';
@@ -9,17 +9,23 @@ import '../../scss/components/counter.scss';
 import Counter from '../common/Counter';
 
 const ItemBasket = ({item}) => {
-    const {id, name, price, count:maxCount, img: imgName, basketCount} = item;
+    const dispatch = useDispatch();
+
+    const {id, name, price, count: maxCount, img: imgName, basketCount} = item;
     const img = `${STATIC_URL}/${imgName}`;
 
-    const [count, setCount] = useState(basketCount);
 
-    const dispatch = useDispatch();
+    const [currentCount, setCurrentCount] = useState(basketCount);
+    const setCount = async (value) => {
+        await setItemCount(id, value);
+        setCurrentCount(value);
+    };
 
     const removeItemBasket = async (item) => {
         dispatch(deleteItemBasket(item.id));
         await deleteFromBasket(item.id);
     }
+
     return (
         <div className="list-block__item item-basket">
             <div className="item-basket__img">
@@ -29,12 +35,11 @@ const ItemBasket = ({item}) => {
             <div className="item-basket__price">{price}</div>
             <Counter
                 className={'item-basket__counter'}
+                count={currentCount}
+                setCount={setCount}
                 maxCount={maxCount}
-                count = {count}
-                setCount = {setCount}
-                id = {id}
             />
-            <div className="item-basket__total">{price * count}</div>
+            <div className="item-basket__total">{price * currentCount}</div>
             <div onClick={() => removeItemBasket(item)} className="item-basket__delete-icon">
                 <img src={deleteIcon} alt="delete"/>
             </div>
