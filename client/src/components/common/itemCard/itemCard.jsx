bf0ab8e9ede7871d3b20d202c7d0dbde56e8023f
Ilya Shishkin <mostbook@mail.ru>
2022-04-30 09:23:42 +0700

    - wrapped img into Link - added loader (appears when added to the basket instead of the button)

--- client/src/components/common/itemCard/itemCard.jsx
+++ client/src/components/common/itemCard/itemCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../scss/components/item-wrapper.scss';
 import InteractionButton from "../templates/buttonTemplates/InteractionButton";
 import {addToBasket} from "../../../http/basketAPI";
@@ -6,33 +6,46 @@ import {STATIC_URL} from "../../../http/consts";
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemBasket } from '../../../store/actions';
 import RubleSign from '../templates/RubleSign';
+import {Link} from "react-router-dom";
+import {paths} from "../../../utils/routes";
+import {HorizontalLoader} from "../templates/horizontalLoader";
 
 function ItemCard({item}) {
     const {id, name, price, count, img: imgName} = item;
     
-    const basketItems = useSelector(store => store.basket.basketItems);
     const dispatch = useDispatch();
+    const basketItems = useSelector(store => store.basket.basketItems);
     const isAuth = useSelector(store => store.isAuth);
+
+    const [isAddingToBasket, setIsAddingToBasket] = useState(false);
     const img = `${STATIC_URL}/${imgName}`;
     
     const addToBasketHandler = async () => {
-        if (!basketItems.find((basketItem) => {
+        const isAlreadyExists = Boolean(basketItems.find((basketItem) => {
             return basketItem.item.id === id;
-        }) && isAuth) {
+        }));
+        if (!isAlreadyExists && isAuth) {
+            setIsAddingToBasket(true);
             dispatch(addItemBasket(item));
-            await addToBasket(id);
+            const isSuccess = await addToBasket(id);
+            setIsAddingToBasket(false);
         }
     };
 
     return (
-        <div className='item-card'>
-            <div className="item-card__image-wrapper">
+        <div
+            className='item-card'
+        >
+            <Link
+                className="item-card__image-wrapper"
+                to={`${paths.ITEM}/${id}`}
+            >
                 <img
                     src={img}
                     className='item-card__image'
                     alt='Изображение товара'
                 />
-            </div>
+            </Link>
             <div className="item-card__description">
                 <div className="item-card__name">
                     {name}
@@ -41,10 +54,14 @@ function ItemCard({item}) {
                     {price}<RubleSign marginLeft = {"3"}/>
                 </div>
             </div>
-            <InteractionButton
-                onClick={addToBasketHandler}
-                value={'В корзину'}
-            />
+            {
+                isAddingToBasket
+                    ? <HorizontalLoader/>
+                    : <InteractionButton
+                        onClick={addToBasketHandler}
+                        value={'В корзину'}
+                    />
+            }
         </div>
     )
 }
