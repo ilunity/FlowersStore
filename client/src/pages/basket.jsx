3599e820900a4e969cae3ce8fecef6c3d7e47458
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-09 21:05:50 +0300

    removed unnecessary imports

--- client/src/pages/basket.jsx
+++ client/src/pages/basket.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
-import InteractionButton from '../components/common/templates/buttonTemplates/InteractionButton';
-import RubleSign from '../components/common/templates/RubleSign';
-import SliderBox from '../components/common/slider/SliderBox';
-import {paths} from '../utils/routes';
-import BasketItems from '../components/common/basketContents/BasketItems';
-import BasketTotalSum from '../components/common/basketContents/BasketTotalSum';
+import React from "react";
+import SliderBox from "../components/common/slider/SliderBox";
+import BasketItems from "../components/common/basketContents/BasketItems";
+import BasketTotalSum from "../components/common/basketContents/BasketTotalSum";
 
 const Basket = () => {
-    
     return (
-        <main className='main'>
+        <main className="main">
             <div className="main__basket basket">
                 <div className="basket__container container">
                     <div className="basket__title title">Корзина</div>
                     <div className="basket__body">
-                        <BasketItems/>
-                        <BasketTotalSum/>
+                        <BasketItems />
+                        <BasketTotalSum />
                     </div>
-                    <SliderBox/>
+                    <SliderBox />
                 </div>
             </div>
         </main>
